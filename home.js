e0647d332511299a5a2285e89fd2409aa2a4eb26
Irene Waweru <85219856+irenenjoki@users.noreply.github.com>
2023-06-30 15:00:41 +0300

    Add files via upload

---
+++ home.js
@@ -0,0 +1,15 @@
+// JavaScript code to get the current date and year
+ var currentDate = new Date();
+ var year = currentDate.getFullYear();
+
+
+ // Function to update the date and year
+ function updateDate() {
+   document.getElementById('current-year').textContent = "© SYLVIA. All right Reserved " + year;
+ }
+
+ // Update the date and year initially
+ updateDate();
+
+ // Update the date and year every second
+ setInterval(updateDate, 1000);
